cb4d3fe28d776f30206de1b9085babe1721cac8a
theijhay <olawaleisaacjohn@gmail.com>
2024-02-26 19:39:23 +0100

    an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,32 +6,21 @@ from models import storage
 from models import amenity, city, place, review, state, user
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """Returns a JSON response with status OK."""
-    return jsonify({"status": "OK"})
+    ''' return status '''
 
+    return (jsonify({"status": "OK"}))
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """Returns statistics."""
 
     # Get the counts from your data storage
-    amenity_count = len(storage.all(amenity))
-    city_count = len(storage.all(city))
-    place_count = len(storage.all(place))
-    review_count = len(storage.all(review))
-    state_count = len(storage.all(state))
-    user_count = len(storage.all(user))
-
-    # Construct the statistics data
-    stat_data = {
-        "amenities": amenity_count,
-        "cities": city_count,
-        "places": place_count,
-        "reviews": review_count,
-        "states": state_count,
-        "users": user_count
-    }
-
-    return jsonify(stat_data)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
